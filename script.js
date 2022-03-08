708bcaff67a9dfdadcec25c39d1da1d7b34849c7
Ethan Hearn <hearn@hearn.nomads.utk.edu>
2022-03-08 13:16:36 -0500

    Implement game() to play a best of 5 game

--- script.js
+++ script.js
@@ -10,31 +10,62 @@ const playRound = (playerSelection, computerSelection=computerPlay()) => {
 
     // Determine if there is a draw 
     if (computerSelection == playerSelection) {
-        return "Tie!"
+        console.log("It's a tie!")
+        return "Tie!";
     
     // Determine winner if computer selected Rock
     } else if (computerSelection == 'rock') {
         if (playerSelection == 'paper') {
-            return "You Win! Paper beats rock."
+            console.log("You Win! Paper beats rock.");
+            return 'you';
         } else if (playerSelection == 'scissors') {
-            return "You Lose! Rock beats scissors."
+            console.log("You Lose! Rock beats scissors.");
+            return 'comp';
         }
     
     // Determine winner if computer selected Paper
     } else if (computerSelection == 'paper') {
         if (playerSelection == 'scissors') {
-            return "You Win! scissors beats paper."
+            console.log("You Win! scissors beats paper.");
+            return 'you';
         } else if (playerSelection == 'rock') {
-            return "You Lose! Paper beats rock."
+            console.log("You Lose! Paper beats rock.");
+            return 'comp';
         }
 
     // Determine winner if computer selected Scissors
     } else if (computerSelection == 'scissors') {
         if (playerSelection == 'rock') {
-            return "You Win! Rock beats scissors."
+            console.log("You Win! Rock beats scissors.");
+            return 'you';
         } else if (playerSelection == 'paper') {
-            return "You Lose! Scissors beats paper."
+            console.log("You Lose! Scissors beats paper.");
+            return 'comp';
         }
     }
     
+}
+
+const game = () => {
+    let compScore = 0;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        if(playRound(window.prompt()) == 'you') {
+            playerScore += 1;
+        } else {
+            compScore += 1;
+        }
+    }
+
+    if (compScore > playerScore) {
+        console.log('You lose!');
+        return 'You lose';
+    } else if (compScore < playerScore) {
+        console.log('You win!');
+        return 'You win';
+    } else {
+        console.log('Tie!');
+        return "It's a tie!";
+    }
 }
